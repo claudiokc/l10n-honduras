1f74b0cb93ac782bab17ea89476e6b95c437c652
Claudio Caceres <catocaceres@hotmail.com>
2017-01-05 22:03:12 -0600

    Updating manifest

--- __manifest__.py
+++ __manifest__.py
@@ -3,20 +3,20 @@
     'name': "l10n-honduras",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Accounting Localization for 2016 Honduran tax policies 
+        """,
 
     'description': """
-        Long description of module's purpose
+        Hoduras accounting module
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Claudio J. Cáceres",
+    'website': "http://www.esmeraldalabs.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Localization',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
